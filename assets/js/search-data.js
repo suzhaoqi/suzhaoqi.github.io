f8f194e0efc872d322f43397df62286cbd31e184
suzhaoqi <140143200+suzhaoqi@users.noreply.github.com>
2026-08-26 02:00:19 +0000

    Deploying to gh-pages from @ suzhaoqi/suzhaoqi.github.io@d91b1ac953d76ab7acbec6601b3880464a947f95 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -34,29 +34,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "books-the-godfather",
+        },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "",handler: () => {
@@ -97,35 +75,10 @@ ninja.data = [{
           section: "News",},{id: "news-paper-thermosplat-cross-modal-3d-gaussian-splatting-with-feature-modulation-and-geometry-decoupling-1st-author-is-accepted-by-neurocomputing",
           title: 'Paper “ThermoSplat: Cross-Modal 3D Gaussian Splatting with Feature Modulation and Geometry Decoupling” (1st...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-caphy-capturing-physical-properties-for-animatable-human-avatars",
+          section: "News",},{id: "projects-caphy-capturing-physical-properties-for-animatable-human-avatars",
           title: 'CaPhy: Capturing Physical Properties for Animatable Human Avatars',
           description: "ICCV 2023",
-          section: "",handler: () => {
+          section: "Projects",handler: () => {
               window.location.href = "/projects/CaPhy/";
             },},{
         id: 'social-email',
